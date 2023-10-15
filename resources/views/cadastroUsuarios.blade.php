0a20c5ef9f91b0516f220fe36e3adb397a196139
Daniel123457 <128873792+Daniel123457@users.noreply.github.com>
2023-10-15 14:43:18 -0300

    atualizado

---
+++ resources/views/cadastroUsuarios.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@3.7.7/dist/full.css" rel="stylesheet" type="text/css" />
+    <script src="https://cdn.tailwindcss.com"></script>
+
+    <title>Cadastro de Usuarios</title>
+</head>
+<body>
+  <h1 class="w-full text-center font-bold">Cadastro de Usuários</h1>
+
+  <form action="{{route('salva-usuario')}}" method="post" class="form-control max-w-md m-auto">
+
+  @csrf
+
+    <div class="form-control">
+      <label class="label">
+      <span class="label-text font-bold">Usuário</span>
+      </label>
+      <input name="usuario" type="text" placeholder="Usuário" class="input input-bordered" required />
+    </div>
+
+    <div class="form-control">
+      <label class="label">
+      <span class="label-text font-bold">Nome</span>
+      </label>
+      <input name="nome" type="text" placeholder="Nome" class="input input-bordered" required />  
+    </div>
+
+    <div class="form-control">
+      <label class="label">
+      <span class="label-text font-bold">E-mail</span>
+      </label>
+      <input name="email" type="email" placeholder="E-mail" class="input input-bordered" required />  
+    </div>
+    
+    <div class="form-control">
+      <label class="label">
+      <span class="label-text font-bold">Senha</span>
+      </label>
+      <input name="senha" type="password" placeholder="Senha" class="input input-bordered" required />  
+    </div>
+
+    <div class="form-control">
+      <label class="label">
+      <span class="label-text font-bold">Bio</span>
+      </label>
+      <input name="bio" type="text" placeholder="Bio" class="input input-bordered" required />  
+    </div>
+
+    <br>  
+    <button class="btn btn-success">Salvar</button>
+
+  </form>
+
+ 
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/teste', function () {
+    return ('testado');
+});
+
+Route::get('/lista-usuarios', function () {
+    $usuarios = User::all();
+
+    return view('listaUsuarios', compact('usuarios'));
+})->name('lista-usuario');
+
+
+Route::view('/cadastroUsuarios', 'cadastroUsuarios');
+
+
+Route::post("/salva-usuario", function(Request $request){
+    $usuario = new User();
+    $usuario->usuario = $request->input('usuario');
+    $usuario->bio = $request->input('bio');
+    $usuario->name = $request->input('nome');
+    $usuario->email = $request->input('email');
+    $usuario->password = $request->input('senha');
+    $usuario->save();
+
+    return "Salvo com sucesso!!";
+})->name('salva-usuario');
